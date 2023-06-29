227d3236da8f94b22a07e071b70845966bd31ca9
Jonathan Tay <jontay999@gmail.com>
2023-06-29 13:48:52 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jamcoders-2023',
+    version='1.0.0',
+    author='Jamcoders',
+    author_email='your@email.com',
+    description='Utilities for JamCoders',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
