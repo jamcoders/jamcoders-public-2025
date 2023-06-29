2c0119c2030b0834626b1fb8add8dd16d2993690
Micah Carroll <mdc@berkeley.edu>
2023-06-29 14:03:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jamcoders-2023',
+    name='jamcoders_2023',
     version='1.0.0',
     author='Jamcoders',
     author_email='your@email.com',
