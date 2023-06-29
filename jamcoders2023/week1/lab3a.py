f7962e061a3f2babc18be5ccc91d7a49c0ece07d
Jonathan Tay <jontay999@gmail.com>
2023-06-29 15:16:47 -0500

    week1 lab3a

---
+++ jamcoders2023/week1/lab3a.py
@@ -0,0 +1,15 @@
+# a specific function to check answers without revealing the answer
+def check_answers(answer, correct):
+    if correct == answer:
+        print("Your answer is correct!")
+    else:
+        print(f"Your answer: '{answer}' is wrong :( try again!")
+
+
+def check_7_1a(ans):
+    answer = "10 5"
+    check_answers(ans, answer)
+
+def check_7_1b(ans):
+    answer = "0"
+    check_answers(ans,answer)
