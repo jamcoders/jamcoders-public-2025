f96916f79145cba95ead41b99fc421d4eef87772
micah <mdc@berkeley.edu>
2023-06-30 14:19:21 -0500

    updated assert_equal

--- jamcoders/base_utils.py
+++ jamcoders/base_utils.py
@@ -1,8 +1,8 @@
 import base64
 
-# For comparison of answers
-def assert_equal(want, got):
 
+# For comparison of answers
+def assert_equal(got, want):
     if want == got or (type(want) == float and type(got) == float and abs(want - got) < 0.001):
         print("Test case passed.")
         return
@@ -14,18 +14,20 @@ def assert_equal(want, got):
     print("-------------------------------------")
     print()
 
+
 # Encodes ASCII string to base64
 def encode_base64(str):
     message = str
-    message_bytes = message.encode('ascii')
+    message_bytes = message.encode("ascii")
     base64_bytes = base64.b64encode(message_bytes)
-    base64_message = base64_bytes.decode('ascii')
+    base64_message = base64_bytes.decode("ascii")
     return base64_message
+
 
 # Decodes base64 to ASCII string
 def decode_base64(b64):
     base64_message = b64
-    base64_bytes = base64_message.encode('ascii')
+    base64_bytes = base64_message.encode("ascii")
     message_bytes = base64.b64decode(base64_bytes)
-    message = message_bytes.decode('ascii')
+    message = message_bytes.decode("ascii")
     return message
