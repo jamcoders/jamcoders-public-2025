6b60502010399fec70cf7fc631254a8132b6d782
Ice Unjitwattana <pawat.unj@gmail.com>
2023-06-30 10:21:21 -0500

    b64 encoder decoder

--- jamcoders/base_utils.py
+++ jamcoders/base_utils.py
@@ -1,3 +1,4 @@
+import base64
 
 def assert_equal(want, got):
 
@@ -11,3 +12,17 @@ def assert_equal(want, got):
     print(f"Got:  {repr(got)} (type: {type(got).__name__})")
     print("-------------------------------------")
     print()
+
+def encode_base64(str):
+    message = str
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
+
+def decode_base64(b64):
+    base64_message = b64
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    return message
