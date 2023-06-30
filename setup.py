8d62e096d9ccd536a981987593a23f15cea98e9f
Emaan Hariri <emaanhariri@Emaans-MacBook-Pro.local>
2023-06-30 12:15:44 -0500

    change name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jamcoders2023',
-    version='1.0.0',
+    name='jamcoders',
+    version='1.0.1',
     author='Jamcoders',
     author_email='jamcoders@jamcoders.com',
     description='Utilities for JamCoders',
