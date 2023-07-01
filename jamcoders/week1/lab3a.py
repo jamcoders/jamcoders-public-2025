4a2754ecfd257d69a34b7ac6be3e24f359c3b81d
Jonathan Tay <jontay999@gmail.com>
2023-06-30 19:11:09 -0500

    change qn number

--- jamcoders/week1/lab3a.py
+++ jamcoders/week1/lab3a.py
@@ -6,10 +6,10 @@ def check_answers(answer, correct):
         print(f"Your answer: '{answer}' is wrong :( try again!")
 
 
-def check_7_1a(ans):
+def check_6_1a(ans):
     answer = "10 5"
     check_answers(ans, answer)
 
-def check_7_1b(ans):
+def check_6_1b(ans):
     answer = "0"
     check_answers(ans,answer)
