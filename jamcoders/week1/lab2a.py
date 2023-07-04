5c492469408b53dfccf6edec1a36366226dedd32
Jonathan Tay <jontay999@gmail.com>
2023-07-03 22:27:18 -0500

    add question numbers

--- jamcoders/week1/lab2a.py
+++ jamcoders/week1/lab2a.py
@@ -1,24 +1,24 @@
 # For answer checking without revealing the answer
-def check_answers(answer, correct):
+def check_answers(answer, correct, num):
     if correct == answer:
-        print("Your answer is correct!")
+        print(f"Your answer to Question {num} is correct!")
     else:
         print(f"Your answer: '{answer}' is wrong :( try again!")
 
 
-def answer_true(ans):
+def answer_true(ans,num):
     answer = True
-    check_answers(ans, answer)
+    check_answers(ans, answer,num)
 
-def answer_false(ans):
+def answer_false(ans,num):
     answer = False
     check_answers(ans, answer)
 
-def check_answer1(ans): answer_false(ans)
-def check_answer2(ans): answer_false(ans)
-def check_answer3(ans): answer_true(ans)
-def check_answer4(ans): answer_true(ans)
-def check_answer5(ans): answer_true(ans)
-def check_answer6(ans): answer_false(ans)
-def check_answer7(ans): answer_true(ans)
-def check_answer8(ans): answer_true(ans)
+def check_answer1(ans): answer_false(ans,1)
+def check_answer2(ans): answer_false(ans,2)
+def check_answer3(ans): answer_true(ans,3)
+def check_answer4(ans): answer_true(ans,4)
+def check_answer5(ans): answer_true(ans,5)
+def check_answer6(ans): answer_false(ans,6)
+def check_answer7(ans): answer_true(ans,7)
+def check_answer8(ans): answer_true(ans,8)
