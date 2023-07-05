ab3d57d48e0daa5de8c8761b6df9cd0258400d17
Jonathan Tay <jontay999@gmail.com>
2023-07-04 20:40:19 -0500

    finish answer checking w1lab3a

--- jamcoders/week1/lab3a.py
+++ jamcoders/week1/lab3a.py
@@ -1,24 +1,10 @@
 # For answer checking without revealing the answer
-def check_answers(answer, correct):
-    if correct == answer:
-        print("Your answer is correct!")
-    else:
-        print(f"Your answer: '{answer}' is wrong :( try again!")
-
 def check_answers_with_num(answer, correct, num):
     if correct == answer:
         print(f"Your answer to Question {num} is correct!")
     else:
         print(f"Your answer to Question {num}: '{answer}' is wrong :( try again!")
 
-
-def check_6_1a(ans):
-    answer = "10 5"
-    check_answers(ans, answer)
-
-def check_6_1b(ans):
-    answer = "0"
-    check_answers(ans,answer)
 
 def create_check_answer(correct, num):
     def check_fn(ans):
@@ -31,4 +17,11 @@ check_answer_3c = create_check_answer(3, '3c')
 check_answer_3d = create_check_answer(1, '3d')
 check_answer_4a = create_check_answer(9, '4a')
 check_answer_4b = create_check_answer(3, '4b')
-check_answer_6 = create_check_answer(25, '6')
+
+check_6_1_100 = create_check_answer(1000,'6.1 (100)')
+check_6_1_123456 = create_check_answer(1234566,'6.1 (123456)')
+check_6_1_420 = create_check_answer(4200,'6.1 (420)')
+
+check_answer_8_1a = create_check_answer('10 5', '8.1 (a)')
+check_answer_8_1b = create_check_answer('0', '8.1 (b)')
+
