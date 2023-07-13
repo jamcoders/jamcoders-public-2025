ecff64161bddbcc3151939609768dc14a057df7a
Ice Unjitwattana <pawat.unj@gmail.com>
2023-07-13 00:31:14 -0500

    added orr_counter

--- jamcoders/week2/labw2d4a.py
+++ jamcoders/week2/labw2d4a.py
@@ -4,8 +4,13 @@ def check_answers(answer, correct, num):
     else:
         print(f"Your answer to Question {num}: '{answer}' is wrong :( try again!")
 
-def warmup_check1(answer):
+def warmup_check1(answer, orr_counter):
     check_answers(answer, 14, "WWPD #1")
+    if orr_counter == 1:
+        print("Congrats! You got it on your first try. Call Orr over to congratulate you :)")
 
-def warmup_check2(answer):
-    check_answers(answer, [3, 2, 1, 0, 1, 2, 3], "WWPD #2")
+def warmup_check1(answer):
+    check_answers(answer, 0, "WWPD #2")
+
+def warmup_check3(answer):
+    check_answers(answer, [3, 2, 1, 0, 1, 2, 3], "WWPD #3")
