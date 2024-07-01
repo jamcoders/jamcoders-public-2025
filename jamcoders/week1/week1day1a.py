5b66a8469375f46f8ae00d2bae76770f2ab9e336
Reginald Frank <reginaldfrank77@berkeley.edu>
2024-07-01 08:38:36 -0500

    Day 1 changes

--- jamcoders/week1/week1day1a.py
+++
@@ -1,2 +0,0 @@
-def print_question(question: str) -> None:
-    print(f'\n{question}', end=' ')
--- jamcoders/week1/week1day1b.py
+++ jamcoders/week1/week1day1b.py
@@ -1,14 +1,2 @@
-def check_expression1(expression):
-    print(f'   A    | Your Answer ')
-    print(f'--------|-------------')
-    print(f'  True  |  {expression(True)}  ')
-    print(f'  False |  {expression(False)}   ')
-
-
-def check_expression2(expression):
-    print(f'   A    |    B    | Your Answer ')
-    print(f'--------|---------|--------------')
-    print(f'  True  |  True   |  {expression(True, True)}  ')
-    print(f'  True  |  False  |  {expression(True, False)}  ')
-    print(f'  False |  True   |  {expression(False, True)}  ')
-    print(f'  False |  False  |  {expression(False, False)}  ')
+def print_question(question: str) -> None:
+    print(f'\n{question}', end=' ')
