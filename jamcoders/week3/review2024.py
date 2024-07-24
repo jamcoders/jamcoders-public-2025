6669ff197377e89d1b14afc34b75e00dffa566c1
Tarun Amarnath <tarunamarnath@gmail.com>
2024-07-23 19:45:07 -0500

    Simplify review checking code

--- jamcoders/week3/review2024.py
+++ jamcoders/week3/review2024.py
@@ -1,9 +1,4 @@
 def create_solution_functions():
-    import sympy
-    from sympy import Symbol, Function, log, sympify
-
-    n = Symbol('n')
-    O = Function('O')
 
     solutions = {
         "1_1" : 80,
@@ -36,39 +31,11 @@ def create_solution_functions():
 
     def make_solution_function(correct_answer):
         def solution_function(answer):
-            import math
-            n = Symbol('n')
-            answer = sympify(answer).replace(O, lambda x: x)
-            correct = correct_answer.replace(O, lambda x: x)
-            if correct.equals(answer):
+            if correct_answer == answer:
                 print("Test case passed!")
-                return
-
-            results = []
-            for exp in range(10, 110, 10):
-                val = 10 ** exp
-                ratio = (answer.subs(n, val) / correct.subs(n, val))
-                results.append(log(ratio).evalf())
-            inf, sup = min(results), max(results)
-            if math.isclose(inf, sup):
-                if math.isclose(inf, 0):
-                    print("Test case passed!")
-                else:
-                    print("---------------- Test case failed. ----------------")
-                    print(f"Very close! The answer you provided, {repr(answer)},")
-                    print("differs from the correct answer by a constant factor.")
-                    print("---------------------------------------------------")
-                    print()
             else:
-                print("---------------- Test case failed. ----------------")
-                if results[-1] < 1:
-                    print(f"The answer you provided, {repr(answer)}, grows ")
-                    print("more slowly than the correct answer.")
-                elif results[-1] > 1:
-                    print(f"The answer you provided, {repr(answer)}, grows ")
-                    print("more quickly than the correct answer.")
-                print("---------------------------------------------------")
-                print()
+                print(f"Your answer is wrong :( try again!")
+
         return solution_function
 
     # globals_ = globals()
