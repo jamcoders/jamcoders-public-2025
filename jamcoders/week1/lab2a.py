2fa0fede14b6bfe4d3e54b400b8279283b59f8ba
adrianna876 <adrianna.b.clashing@gmail.com>
2025-06-26 12:10:43 -0500

    Update lab2a.py

--- jamcoders/week1/lab2a.py
+++ jamcoders/week1/lab2a.py
@@ -5,38 +5,24 @@ def check_answers(answer, correct, num):
     else:
         print(f"Your answer to Question {num}: '{answer}' is wrong :( try again!")
 
+def check_answer_1_1(answer):
+    check_answers(answer, "B", "1.1")
 
-def answer_true(ans,num):
-    answer = True
-    check_answers(ans, answer,num)
+def check_answer_1_2(answer):
+    check_answers(answer, "C", "1.2")
 
-def answer_false(ans,num):
-    answer = False
-    check_answers(ans, answer, num)
+def check_answer_2_0(answer):
+    check_answers(answer, "It's not too hot.", "2.0")
 
-# Option 1: Trying it for now
-def check_answer1(ans): answer_false(ans,1)
-def check_answer2(ans): answer_false(ans,2)
-def check_answer3(ans): answer_true(ans,3)
-def check_answer4(ans): answer_true(ans,4)
-def check_answer5(ans): answer_true(ans,5)
-def check_answer6(ans): answer_false(ans,6)
-def check_answer7(ans): answer_true(ans,7)
-def check_answer8(ans): answer_true(ans,8)
+def check_answer_2_4(answer1, answer2):
+    answers = [answer1] + [answer2]
+    check_answers(answers, [2,1], "2.4")
 
-def check_answer_2_1(ans): check_answers(ans, "coder", "2.1")
-def check_answer_2_2(ans): check_answers(ans, "starfruit", "2.2")
-def check_answer_2_3(ans): check_answers(ans, ['orange', 'banana', 'pear'], "2.3")
+def check_answer_3_1(answer_A, answer_B, answer_C, answer_D):
+    answers = [answer_A] + [answer_B] +[answer_C] +[answer_D] 
+    check_answers(answers, [2,1,4,3], "3.1")
 
-# Option 2
-def check_answer_0_2(ans):
-    answers = [False, False, True, True, True, False, True,True]
-    for i in range(len(answers)):
-        check_answers(ans[i],answers[i], i+1)
 
-# Trying this
-def check_answer_3_4(ans):
-    answers = [False,True,False,False,True,True,True]
-    for i in range(len(answers)):
-        check_answers(ans[i],answers[i], i+1)
-
+def check_answer_3_4(answer):
+    check_answers(answer, "I am cool." "3.1")
+ 
