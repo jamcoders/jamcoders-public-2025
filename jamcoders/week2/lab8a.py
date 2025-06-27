88bb5470074fae1b2e1fffb690e2d3324551777f
Adrianna <adrianna.b.clashing@gmail.com>
2025-06-27 12:48:34 -0500

    Updated lab8a

--- jamcoders/week2/lab8a.py
+++ jamcoders/week2/lab8a.py
@@ -12,8 +12,8 @@ def check_answer1(a):
 def check_answer2(a, b, c):
     check_answers([a, b, c], ["a", "1", "x is not in L"], "1.2")
 
-def check_answer3(a, b, c, d):
-    check_answers([a, b, c, d], ["x is not in L", "x is in L", 9002, 2000], "1.3")
+def check_answer3(a, b, c, d, e):
+    check_answers([a, b, c, d, e], ["x is not in L", "x is in L", 9002, 2000, -1], "1.3")
 
 def check_answer4(a, b, c, d, e):
     check_answers([a, b, c, d, e], [5, 17, 14, 9, 120], "1.4")
