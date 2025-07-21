718e02dbdc5b36a9bd8422a24dabc4009243e8c4
Samuel Bobick <samuelbobick@gmail.com>
2025-07-21 13:06:31 -0500

    naming changes

--- jamcoders/week4/labw4d2a.py
+++ jamcoders/week4/labw4d2a.py
@@ -1,47 +1,6 @@
-import matplotlib.pyplot as plt
-import networkx as nx
-import pydot
-from networkx.drawing.nx_pydot import graphviz_layout
-
-
-def generate_graph(weighted_adj_list):
-    G = nx.DiGraph()
-    G = add_edges(G, weighted_adj_list)
-    return G
-
-
-def add_edges(G, data):
-    for word1 in data:
-        for word2 in data[word1]:
-            G.add_edge(word1, word2, weight=data[word1][word2])
-    return G
-
-
-def plot_graph(G):
-    # Use graphviz layout with pydot (top-down)
-    pos = graphviz_layout(G, prog="dot")  # 'dot' gives hierarchical top-down layout
-
-    # Draw nodes and edges
-    plt.figure(figsize=(8, 6))
-    # TODO arrows wider and thinner (proportional to probability)
-    nx.draw(
-        G,
-        pos,
-        with_labels=True,
-        node_size=2000,
-        node_color="lightblue",
-        arrows=True,
-        arrowstyle="->",
-        arrowsize=20,
-        font_size=12,
-    )
-
-    # Draw edge labels
-    edge_labels = nx.get_edge_attributes(G, "weight")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10)
-
-    plt.title("Bigram Probabilities")
-    plt.show()
+import re
+import pickle
+import urllib.request
 
 # For answer checking without revealing the answer
 def check_answers_with_num(answer, correct, num):
@@ -72,6 +31,96 @@ def create_check_answer_multi(correct):
     return check_fn
 
 
-check_answer_1_1 = create_check_answer_multi(
-    [8, 8, 3, False]
+# Fuzzy check for single numerical answer with tolerance
+def check_answers_with_num_fuzzy(answer, correct, num, tol):
+    if abs(answer - correct) <= tol:
+        print(f"Your answer to Question {num} is correct!")
+    else:
+        print(f"Your answer to Question {num}: '{answer}' is wrong :( try again!")
+
+
+def create_check_answer_fuzzy(correct, num, tol):
+    def check_fn(ans):
+        check_answers_with_num_fuzzy(ans, correct, num, tol)
+
+    return check_fn
+
+
+# Fuzzy check for multiple numerical answers with tolerances
+def check_answers_with_num_multi_fuzzy(answer_list, correct_list, tolerances):
+    all_correct = True
+    for ans, correct, tol in zip(answer_list, correct_list, tolerances):
+        if abs(ans - correct) > tol:
+            all_correct = False
+            break
+    if all_correct:
+        print(f"All your answers are correct!")
+    else:
+        print(f"At least one of your answers is wrong :( try again!")
+
+
+def create_check_answer_multi_fuzzy(correct_list, tolerances):
+    def check_fn(ans_list):
+        check_answers_with_num_multi_fuzzy(ans_list, correct_list, tolerances)
+
+    return check_fn
+
+
+def clean(line):
+    """
+    Utility functon for NLP project.
+
+    Clean up a line of text by:
+    - Replacing ellipses with single periods
+    - Removing quotes
+    - Removing tabs
+    - Make text lowercase
+    - Smush standalone apostrophes with surrounding words
+
+    Args:
+        line (str): Input string to clean
+
+    Returns:
+        str: Cleaned string
+    """
+
+    # Replace ellipses (...) with single period
+    line = line.replace("...", ".")
+    line = line.replace("….", ".")  # Handle other ellipsis variations
+
+    # Remove tabs
+    line = line.replace("\t", " ")
+
+    # Remove quote
+    line = line.replace('"', "")
+    line = line.replace("`", "'")
+    line = line.replace("’", "'")
+
+    # Smush standalone apostrophes:
+    # For example: convert "word ' word" to "word'word"
+    # Use regex to replace occurrences of space + apostrophe + space with just apostrophe
+    line = re.sub(r"\s'\s", "'", line)
+
+    # Clean up multiple spaces again (because we replaced tabs and smushed apostrophes)
+    line = " ".join(line.split())
+
+    # Lowercase
+    line = line.lower()
+
+    return line
+
+def load_dataset():
+    url = "https://raw.githubusercontent.com/jamcoders/jamcoders-public-2025/main/jamcoders/week4/data/corpus.pkl"
+
+    with urllib.request.urlopen(url) as response:
+        corpus = pickle.load(response)
+        
+    return corpus
+
+check_answer_1_4 = create_check_answer_multi_fuzzy(
+    [19790, 93.51258755002858], [1000, 5]
 )
+check_answer_3_1 = create_check_answer_multi(
+    [["you", "dogs"], 0.4, "you", ["i", "love", "you", "so", "much"]]
+)
+
